e0fc47a9a67acb59f9a263fba19f880f07af180b
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2021-05-12 20:32:30 +0300

    connect to db

--- src/main/java/com/example/Broken_Hammer/LoginServlet.java
+++ src/main/java/com/example/Broken_Hammer/LoginServlet.java
@@ -1,15 +1,24 @@
 package com.example.Broken_Hammer;
 
+import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try {
+            DBManager dbManager = new DBManager();
+            Connection connection = dbManager.getConnection();
+            System.out.println("Ok!");
+        } catch (SQLException | NamingException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
